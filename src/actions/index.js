f92d381509dd9e6909febc578bd4a163d6fd77a5
veselinn16 <veselinn16@gmail.com>
2018-09-20 19:17:29 +0300

    Construct API call URL :wrench:

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = '6ccc0ace1b6dbf357c4770e681eeda02';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+// to be put in reducer
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    
+
+    return {
+        type: FETCH_WEATHER
+    }
+}
