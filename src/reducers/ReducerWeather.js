b9ecd9a70e3ea79f4aeefd6bea797414663f2f03
veselinn16 <veselinn16@gmail.com>
2018-09-21 23:01:24 +0300

    Handle failed requests :wrench:

--- src/reducers/ReducerWeather.js
+++ src/reducers/ReducerWeather.js
@@ -1,6 +1,10 @@
 import { FETCH_WEATHER } from '../actions/index'
 
 export default function(state = [], action) {
+    if(action.error) {
+        alert('There is no such city.');
+        return state;
+    }
     switch(action.type) {
         case FETCH_WEATHER:
             return [action.payload.data, ...state];
